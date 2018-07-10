b22fd070083739e718d3b4dc050a3d4f65cdbe88
roman <r.maximov2012@yandex.ru>
2018-07-11 02:07:50 +0300

    Конфиг webpack + favicon.

---
+++ webpack.config.js
@@ -0,0 +1,107 @@
+const path = require('path');
+const argv = require('yargs').argv;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = argv.mode === 'development';
+const isProduction = !isDevelopment;
+const distPath = path.join(__dirname, '/public');
+
+const config = {
+  entry: {
+    main: './source/scripts/project/index.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: distPath
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [{
+      test: /\.html$/,
+      use: 'html-loader'
+    }, {
+      test: /\.(js|jsx)/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }]
+    }, {
+      test: /\.sass$/,
+      exclude: /node_modules/,
+      use: [
+        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            minimize: isProduction
+          }
+        },
+        'sass-loader',
+        'resolve-url-loader'
+      ]
+    }, {
+      test: /\.(gif|png|jpe?g|svg|ico)$/i,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name][hash].[ext]'
+        }
+      }, {
+        loader: 'image-webpack-loader',
+        options: {
+          mozjpeg: {
+            progressive: true,
+            quality: 70
+          }
+        }
+      },
+      ],
+    }, {
+      test: /\.(eot|svg|ttf|woff|woff2)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name][hash].[ext]'
+        }
+      },
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './source/views/index.html'
+    })
+  ],
+  optimization: isProduction ? {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: true,
+        uglifyOptions: {
+          compress: {
+            inline: false,
+            warnings: false,
+            drop_console: true,
+            unsafe: true
+          },
+        },
+      }),
+    ],
+  } : {},
+  devServer: {
+    contentBase: distPath,
+    port: 9000,
+    compress: true,
+    open: true
+  }
+};
+module.exports = config;
